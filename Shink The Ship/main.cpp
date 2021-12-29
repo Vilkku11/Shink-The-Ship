8249103a811152998be981fd8d8acc7f9fe58387
William Reima <william.reima@gmail.com>
2021-12-29 21:53:10 +0200

    Add project files.

---
+++ Shink The Ship/main.cpp
@@ -0,0 +1,18 @@
+#include "SFML/Graphics.hpp"
+int main() {
+
+
+	sf::RenderWindow window(sf::VideoMode(1280, 720), "testi");
+	sf::Event e;
+
+	while (window.isOpen()) {
+
+		while (window.pollEvent(e)) {
+
+			if (e.type == sf::Event::Closed) {
+				window.close();
+			}
+		}
+	}
+	return 0;
+}
