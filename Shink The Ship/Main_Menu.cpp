76f08c7cb4f160451b74bf26334d9c1b54fd6e79
William Reima <william.reima@gmail.com>
2021-12-30 01:00:28 +0200

    Working main menu text rendering

--- Shink The Ship/Main_Menu.cpp
+++ Shink The Ship/Main_Menu.cpp
@@ -4,22 +4,22 @@ Main_Menu::Main_Menu(float width, float height)
 {
 	if (!font.loadFromFile("Fonts/Gidole-Regular.ttf"))
 	{
-		menu[0].setFont(font);
-		menu[0].setFillColor(sf::Color::White);
-		menu[0].setString("Play");
-		menu[0].setPosition(sf::Vector2f(width / 2, (MAX_ITEMS + 1) * 2));
-
-		menu[1].setFont(font);
-		menu[1].setFillColor(sf::Color::Blue);
-		menu[1].setString("Options");
-		menu[1].setPosition(sf::Vector2f(width / 2, (MAX_ITEMS + 1) * 2));
-
-		menu[2].setFont(font);
-		menu[2].setFillColor(sf::Color::Blue);
-		menu[2].setString("Exit");
-		menu[2].setPosition(sf::Vector2f(width / 2, (MAX_ITEMS + 1) * 3));
-
+	   //Printing error to console
 	}
+	menu[0].setFont(font);
+	menu[0].setFillColor(sf::Color::White);
+	menu[0].setString("Play");
+	menu[0].setPosition(sf::Vector2f(width / 2, (height/2)+(-50)));
+
+	menu[1].setFont(font);
+	menu[1].setFillColor(sf::Color::Blue);
+	menu[1].setString("Options");
+	menu[1].setPosition(sf::Vector2f(width / 2, (height / 2)));
+
+	menu[2].setFont(font);
+	menu[2].setFillColor(sf::Color::Blue);
+	menu[2].setString("Exit");
+	menu[2].setPosition(sf::Vector2f(width / 2, (height / 2) +50));
 }
 
 void Main_Menu::draw(sf::RenderWindow& window)
